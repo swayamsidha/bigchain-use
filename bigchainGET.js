2a8371642091239afd7ba70701805bc2eda34343
swayamsidha <chandan20july1997@gmail.com>
2018-06-06 13:59:53 +0530

    Add files via upload

--- bigchainGET.js
+++ bigchainGET.js
@@ -1,8 +1,11 @@
 var ex = require('express')
 var app = ex()
 var ax = require('axios')
+var big = require('./bigchain_test2')
+//var mongoose = require('mongodb').Client;
+//mongoose.connect('mongodb://root:toor@ds159489.mlab.com:59489/nihaldb')
 //var bigchainRouter = function(app){
-  var query="cde"
+  var query="spark55"
   var id
   var meta=[]
   var pubK
@@ -10,8 +13,30 @@ var ax = require('axios')
   //getting the asset data
   
   
- 
-var abc = "https://test.bigchaindb.com/api/v1/"
+app.set('view engine', 'ejs'); 
+var abc = "http://10.42.0.1:32809/api/v1/"
+app.listen(9191, function(){
+        console.log("app is running")
+})
+//creating asset and transfering it
+/*app.get("/send", function(req, res){
+  res.render('post.ejs')
+})*/
+app.post("/send", function(req, res){
+  var assetdata = {
+    'bicycle': {
+                'serial_number': 'spark55',
+                'manufacturer': 'TI india',
+                'type': 'trans roader',
+                'color': 'red'
+        }
+  
+  }
+  var metadata = {
+    'network' : 'blockchain'
+  }
+  big.createAndTransfer(assetdata, metadata)
+})
 app.get("/", function(req, res){
     ax.get(abc+"assets?search="+query).then(x =>{
     if(assetData == []){
@@ -19,7 +44,7 @@ app.get("/", function(req, res){
     }
     res.send(x.data)
     assetData.push(x.data[0])
-    //console.log(assetData)
+    console.log(assetData)
     })
   })
 //getting transaction data
@@ -32,10 +57,15 @@ app.get("/transaction", function(req, res){
 })
 //accessing metadata
 app.get("/metadata", function(req, res){
-  ax.get(abc+"metadata?search="+meta[0].planet).then(a => {
+  ax.get(abc+"metadata?search="+meta[0].quantity).then(a => {
     res.send(a.data)
   })
 })
-app.listen(8080, function(){
-        console.log("app is running")
+//getting validators in local node
+app.get("/valid", function(req, res){
+  ax.get(abc+"validators/").then(b => {
+    res.send(b.data)
+  })
 })
+//
+
